ad945da17bf8103b4a57261834e8c8fdf73716d0
Mateus Dias <63275582+mateusdiast@users.noreply.github.com>
2024-11-29 20:53:50 -0300

    Initial Commit

---
+++ MyMovies/MyMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMoviesApp.swift
+//  MyMovies
+//
+//  Created by Mateus Dias on 29/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MyMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
